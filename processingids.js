5bd9fdf91097d1caf965914a6b00911ff367130e
LewisSaber <mama040508@gmail.com>
2022-03-19 00:03:47 +0200

    optimise code

--- processingids.js
+++ processingids.js
@@ -1,29 +1,27 @@
 
 
 var allElements = document.querySelectorAll('*[id]')
-var allIds = [];
-for (var i = 0, n = allElements.length; i < n; ++i) {
-  var el = allElements[i];
-  if (el.id) { allIds.push(el.id); }
-}
 let e = {}
 for ( let i = 0, n = allElements.length; i < n; i++)
 {
-e[allIds[i]] = allElements[i];
+e[allElements[i].id] = allElements[i];
 }
 let upgrades = 4;
 let upgradedescription = ["Add Particle ","Increase Particle Speed","Increase Gain Per Hit","Multiply Gain By Hit"]
-let costnames = [" Particles"," Particles"," Particles"," Particles"]
+let costnames = ["particles","particles","particles","particles"]
 let callupgrades = {}
-
+function getCostName(n)
+{
+  return " " + costnames[n].charAt(0).toUpperCase() + costnames[n].slice(1) 
+}
 for(let i = 0; i < upgrades; i++)
 {
 tag = document.createElement("button");
 tag.setAttribute("class", "upgrade");
 tag.setAttribute("id", "upgrade" + i);
-tag.setAttribute("onclick", "callupgrades.buyupgrade"+i+"()")
+tag.setAttribute("onclick", "buyupgrade("+i+")")
 callupgrades[i] = "buyupgrade"+i+"()"
-tag.innerHTML = upgradedescription[i] + '<p class="cost" id="cost'+ i+'">0' + costnames[i] +'</p>' 
+tag.innerHTML = upgradedescription[i] + '<p class="cost" id="cost'+ i+'">0' + getCostName[i] +'</p>' 
 e.upgradesdiv.appendChild(tag);
 }
 
--- upgrade.js
+++ upgrade.js
@@ -1,83 +1,97 @@
 let loading = 0;
 function loadUpgrades() {
   for (let i = 0; i < upgrades; i++) {
-    callupgrades["buyupgrade" + i]();
+    //callupgrades["buyupgrade" + i]();
+    buyupgrade(i);
   }
-  if(game.activeparticles > 50)
-  {
-      e.upgrade0.style.display = "none"
+  if (game.activeparticles > 50) {
+    e.upgrade0.style.display = "none";
   }
-  if(get_pSpeed() > 5)
-  {
-      e.upgrade1.style.display = "none"
+  if (get_pSpeed() > 5) {
+    e.upgrade1.style.display = "none";
   }
   loading = 1;
 }
-function getCost(r)
-{
-switch (r) {
+function getCost(r) {
+  switch (r) {
     case 0:
-       return  4 * Math.pow(game.upgrades[0] + 1,1 + game.upgrades[0]/10)
+      return 4 * Math.pow(game.upgrades[0] + 1, 1 + game.upgrades[0] / 10);
     case 1:
-        return  9 * Math.pow(game.upgrades[1] + 1,1 + game.upgrades[1]/30)
+      return 9 * Math.pow(game.upgrades[1] + 1, 1 + game.upgrades[1] / 30);
     case 2:
-        return   20 * Math.pow(game.upgrades[2] + 1,1 + game.upgrades[2]/90);
+      return 20 * Math.pow(game.upgrades[2] + 1, 1 + game.upgrades[2] / 90);
     case 3:
-        return 10000 * Math.pow(10,game.upgrades[3])
+      return 10000 * Math.pow(10, game.upgrades[3]);
     default:
-
-        break;
-}
+      break;
+  }
 }
 callupgrades = {
   buyupgrade0: function () {
-   cost = getCost(0)
+    cost = getCost(0);
     if (game.particles >= cost && loading == 1) {
       game.upgrades[0] += 1;
       game.particles -= cost;
       addparticle(1);
     }
-    cost = getCost(0)
+    cost = getCost(0);
     e.cost0.innerText = cost.formateNumber() + costnames[0];
-    if(game.activeparticles > 50)
-    {
-        e.upgrade0.style.display = "none"
+    if (game.activeparticles > 50) {
+      e.upgrade0.style.display = "none";
     }
   },
   buyupgrade1: function () {
-    cost = getCost(1)
+    cost = getCost(1);
     if (game.particles >= cost && loading == 1) {
       game.upgrades[1] += 1;
       game.particles -= cost;
-    
     }
-    cost = getCost(1)
+    cost = getCost(1);
     e.cost1.innerText = cost.formateNumber() + costnames[1];
-  
-  if(get_pSpeed() > 5)
-  {
-      e.upgrade1.style.display = "none"
-  }
+
+    if (get_pSpeed() > 5) {
+      e.upgrade1.style.display = "none";
+    }
   },
   buyupgrade2: function () {
-    cost = getCost(2)
+    cost = getCost(2);
     if (game.particles >= cost && loading == 1) {
       game.upgrades[2] += 1;
       game.particles -= cost;
-    
     }
-   cost = getCost(2)
+    cost = getCost(2);
     e.cost2.innerText = cost.formateNumber() + costnames[2];
   },
   buyupgrade3: function () {
-    cost = getCost(3)
+    cost = getCost(3);
     if (game.particles >= cost && loading == 1) {
       game.upgrades[3] += 1;
       game.particles -= cost;
-    
     }
-   cost = getCost(3)
+    cost = getCost(3);
     e.cost3.innerText = cost.formateNumber() + costnames[3];
   },
 };
+function buyupgrade(r) {
+  cost = getCost(r);
+  if (game[costnames[r]] >= cost && loading == 1) {
+    switch (r) {
+      case 0:
+        addparticle(1);
+
+        break;
+      default:
+        break;
+    }
+    game.upgrades[r] += 1;
+    game[costnames[r]] -= cost;
+  }
+  e["cost" + r].innerText = getCost(r).formateNumber() + getCostName(r);
+  if (get_pSpeed() > 5) {
+    e.upgrade1.style.display = "none";
+  }
+  if (game.activeparticles > 50) {
+    e.upgrade0.style.display = "none";
+  }
+}
 loadUpgrades();
